97b1b562035909a8e8731daa252b3f4da0d67354
diodonfrost <diodon.frost@diodonfrost.me>
2019-09-18 21:46:00 +0200

    add terraform 0.12 for 05 example

--- 05-pool-with-multiple-network/00-variables.tf
+++ 05-pool-with-multiple-network/00-variables.tf
@@ -49,8 +49,8 @@ variable "dns_servers" {
 #
 variable "http_vm_params" {
   default = {
-    vcpu           = "2"
-    ram            = "4096"
+    vcpu = "2"
+    ram  = "4096"
     # You can't set a datastore name with interspace
     disk_datastore = "datastore_test"
     disk_size      = "15"
@@ -59,14 +59,15 @@ variable "http_vm_params" {
 
 variable "http_network_params" {
   default = {
-    domain           = "test.local"
-    label            = "http_network"
-    vlan_id          = "1"
-    base_address     = "192.168.1."
-    prefix_length    = "24"
-    gateway          = "192.168.1.254"
+    domain        = "test.local"
+    label         = "http_network"
+    vlan_id       = "1"
+    base_address  = "192.168.1."
+    prefix_length = "24"
+    gateway       = "192.168.1.254"
   }
 }
+
 # Define the root hostname of http instance
 # The root is incrementing for each instance create
 # e.g: a root name with "frt0" will give:
@@ -89,8 +90,8 @@ variable "http_vm_desired_capacity" {
 #
 variable "db_vm_params" {
   default = {
-    vcpu           = "2"
-    ram            = "2048"
+    vcpu = "2"
+    ram  = "2048"
     # You can't set a datastore name with interspace
     disk_datastore = "datastore_test"
     disk_size      = "15"
@@ -99,12 +100,12 @@ variable "db_vm_params" {
 
 variable "db_network_params" {
   default = {
-    domain           = "test.local"
-    label            = "db_network"
-    vlan_id          = "2"
-    base_address     = "192.168.2."
-    prefix_length    = "24"
-    gateway          = "192.168.2.254"
+    domain        = "test.local"
+    label         = "db_network"
+    vlan_id       = "2"
+    base_address  = "192.168.2."
+    prefix_length = "24"
+    gateway       = "192.168.2.254"
   }
 }
 
@@ -123,3 +124,4 @@ variable "db_base_hostname" {
 variable "db_vm_desired_capacity" {
   default = "3"
 }
+
